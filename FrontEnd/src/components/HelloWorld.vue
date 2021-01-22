b1b358e7b0355f78c77c6f6cf2e3297b02edbeaa
Kyle Tanyag <kyletanyag@gmail.com>
2021-01-21 19:58:54 -0500

    Add visual studio to gitignore

--- FrontEnd/src/components/HelloWorld.vue
+++ FrontEnd/src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         Title: {{ m.title }}, Rating: {{ m.rating }}
       </li>
     </ul>
-    <p>
+    <p> 
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
