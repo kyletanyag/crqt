e5fc41e37930df4bc034a9b97408c3718af1fa61
Holden <hstro002@odu.edu>
2021-02-01 13:57:11 -0500

    Preliminary work on Log In system. Completed the bare minimum of Auth0 integration.
    Added Callback and Error pages for Auth0 to redirect to.

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -2,9 +2,30 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <div v-if="!$auth.loading.value">
+        <button v-if="!$auth.isAuthenticated.value" @click="login">Log in</button>
+        <button v-if="$auth.isAuthenticated.value" @click="logout">Log out</button>
+    </div>
   </div>
   <router-view/>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+}
+</script>
 
 <style>
 #app {
--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,25 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home'
+import About from '@/views/About'
+
+import Callback from '@/views/Callback'
+import ErrorPage from '@/views/Error'
+
+import { routeGuard } from '@/auth'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+        beforeEnter: routeGuard
+    },
+    {
+        path: '/callback',
+        name: 'Callback',
+        component: Callback
+    },
+    {
+        path: '/error',
+        name: 'Error',
+        component: ErrorPage,
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
