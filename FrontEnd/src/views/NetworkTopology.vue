bc84f1db2a7bb87e6ce6948a3cae98515828e37a
Gul Ayaz <gayaz001@odu.edu>
2021-02-01 22:33:06 -0500

    Fixed Nav bar, started adding tables to pages

--- FrontEnd/src/views/NetworkTopology.vue
+++ FrontEnd/src/views/NetworkTopology.vue
@@ -1,22 +1,30 @@
 <template>
+
   <div id="table">
   <table id="table" width="100%" border="0">
 <tbody>
       <tr>
-        <td width="25%"><div id="pic1">
+        <td width="100%"><div id="pic1">
           <div>
-            <h5>hmm</h5>
+            <h5>hmmmmmmmmmmmmmmmm</h5>
             <p>Something</p>
             
             </div>
           </div>
           </td>
         </tr>
-      
-      
-      
       </tbody>
       </table>
-  
-</div>
+      </div>
 </template>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;    
+}
+</style>
