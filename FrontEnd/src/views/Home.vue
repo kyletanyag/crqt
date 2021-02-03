61ce8d49f8ad346025e3135bf474e1d0ebb31a82
Kyle Tanyag <kyletanyag@gmail.com>
2021-02-03 01:38:36 -0500

    Integrate vue-csv-import project with bugs

--- FrontEnd/src/views/Home.vue
+++ FrontEnd/src/views/Home.vue
@@ -3,16 +3,19 @@
         <img alt="CRQT logo" src="../assets/logo.png">
         <HelloWorld msg="Welcome to the Cyber Resilience Quantification Tool (CRQT)"
                     msg2="TestMessage" />
+        <demo></demo>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-    import HelloWorld from '@/components/HelloWorld.vue'
+    import HelloWorld from '@/components/HelloWorld.vue';
+    import Demo from '@/components/Demo.vue';
     export default {
         name: 'Home',
         components: {
-            HelloWorld
+            HelloWorld,
+            Demo
         }
     }
 </script>
