3cf0ff5472cfbedf7913f5eef1b59cd6c0499c42
TomLaverghetta <46882694+TomLaverghetta@users.noreply.github.com>
2021-02-03 21:21:53 -0500

    Updated backend

---
+++ BackEnd/API/user_db.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify, request
+from . import db 
+
+main = Blueprint('main', __name__)
+
+@main.route('/verify_user', methods=['POST'])
+def verify_user():
+    request.get_json()
+    ## query with MySQL
+    # username=key, password, email
+
+@main.route('/register', methods=['POST'])
+def register():
+    request.get_json() # who (name, email, organization), why (why they need access)
+    # send msg to admins 
+
+# admin call ONLY
+@main.route('/add_user_init', methods=['POST'])
+def add_user_init():
+    request.get_json() # who (name, email, organization), why (why they need access)
+    # send msg to user
+
+@main.route('/add_user', methods=['POST'])
+def add_user():
+    request.get_json() # 
+    # send msg to user
