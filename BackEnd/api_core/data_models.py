6a00cbdb8d39fc5d980d3a005759c37df0cf1fc4
TomLaverghetta <46882694+TomLaverghetta@users.noreply.github.com>
2021-02-09 09:14:32 -0500

    Removed name fielding in Data models - SQLAlchemy will use object attribute names as name fielding therefore it is not needed

--- BackEnd/api_core/data_models.py
+++ BackEnd/api_core/data_models.py
@@ -7,13 +7,13 @@ from . import db
 
 class Users(db.Model):
     __bind_key__ = 'users'
-    _username = db.Column("username", db.Integer, primary_key=True)
-    _password = db.Column("passwd", db.Integer)
+    _username = db.Column(db.Integer, primary_key=True)
+    _password = db.Column(db.Integer)
 
 class NVD(db.Model):
     __bind_key__ = 'nvd'
-    _cve_id = db.Column("cve_id", db.String(16), primary_key=True)
-    _base_score = db.Column("base_score", db.Float)
-    _exploitabiliy_score = db.Column("exploitabiliy_score_v2", db.Float)
-    _impact_score = db.Column("impact_score_v2", db.Float)
-    _discription = db.Column("discription",db.String(120))
+    _cve_id = db.Column(db.String(16), primary_key=True)
+    _base_score = db.Column(db.Float)
+    _exploitabiliy_score_v2 = db.Column(db.Float)
+    _impact_score_v2 = db.Column(db.Float)
+    _discription = db.Column(db.String(120))
