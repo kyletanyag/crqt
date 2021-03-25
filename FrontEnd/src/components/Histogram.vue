dc6db3f0f2c71b8b670f91970f333ee1b1c7b8d7
Holden Strong <rerofalconwoo@gmail.com>
2021-03-24 21:34:06 -0400

    Histogram now has not option to provide custom labels rather than always generating automatic ones.

--- FrontEnd/src/components/Histogram.vue
+++ FrontEnd/src/components/Histogram.vue
@@ -15,6 +15,10 @@ export default defineComponent({
 
   props: {
     data: Array,
+    binNames: {
+      type: Array(),
+      default: ['automatic']
+    },
     numBins: String, //Vue was giving me an error in SimulationResults.vue, saying that it expected a number but I was passing a string.
     name: String,
     barColor: String
@@ -29,20 +33,29 @@ export default defineComponent({
 
   methods: {
     renderHistogram() {
+
+      // Bin the data
       var histGenerator = d3.histogram();
       histGenerator.domain([0,1]); // Every histogram in this project should range from 0-1, so I've hard-coded it.
       histGenerator.thresholds(this.numBins-1);
       var binnedData = histGenerator(this.data);
       var binSizes = new Array(this.numBins);
+      console.log("binnedData = " + binnedData);
       for (var i = 0; i < this.numBins; i++){
+        console.log("Bin " + i + " = " + binnedData[i]);
         binSizes[i] = binnedData[i].length;
       }
 
-      // Generate bin labels
       var labelArray = new Array(this.numBins);
-      labelArray[0] = "[0, " + (1/this.numBins).toFixed(2).toString() + ")";
-      for (var j = 1; j < this.numBins; j++){
-        labelArray[j] = "[" + ((1/this.numBins)*j).toFixed(2).toString() + ", " + ((1/this.numBins) * (j+1)).toFixed(2).toString() + ")"; // Again, we've hard-coded the domain max as 1
+      if (this.binNames == "automatic"){
+        // Generate bin labels
+        labelArray[0] = "[0, " + (1/this.numBins).toFixed(2).toString() + ")";
+        for (var j = 1; j < this.numBins; j++){
+          labelArray[j] = "[" + ((1/this.numBins)*j).toFixed(2).toString() + ", " + ((1/this.numBins) * (j+1)).toFixed(2).toString() + ")"; // Again, we've hard-coded the domain max as 1
+        }
+      }
+      else{
+        labelArray = this.binNames;
       }
 
       this.renderChart({
