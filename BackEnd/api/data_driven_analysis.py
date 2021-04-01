dd5d381d5f3b4b4a8ccca7a88cd176b611d26b28
TomLaverghetta <46882694+TomLaverghetta@users.noreply.github.com>
2021-04-01 17:09:51 -0400

    Optimized data-driven datastructures

--- BackEnd/api/data_driven_analysis.py
+++ BackEnd/api/data_driven_analysis.py
@@ -9,8 +9,8 @@ import time
 
 # route for LAG generation module
 data_analysis_bp = Blueprint('data_analysis_bp', __name__)
-LAG = {}
-derived_score_computation_time = None
+LAG = []
+derived_score_computation_time = 0.0
 
 # namespace for data-driven objects
 class DataDriven:
@@ -27,27 +27,45 @@ class DataDriven:
         DERIVATION = 1
         DERIVED = 2
 
+    switch = {
+        "FLOW"  : Node_Logic.FLOW,
+        "AND"   : Node_Logic.AND,
+        "LEAF"  : Node_Logic.LEAF,
+        "OR"    : Node_Logic.OR
+    }
+
     # graph data structure (adjenency list) for DataDriven
     class Node:
-        def __init__(self):
+        def __init__(self, index, logic, discription):
+            self.index = index                      # node index
             self.derived_score = [1.0,1.0,1.0]      # base, exploitability, impact scores
-            self.description = str()                # node description
+            self.description = discription          # node description
             self.node_type = None                   # type of node  
-            self.node_logic = None                  # node relationship 
+            self.node_logic = DataDriven.switch[logic]# node relationship 
             self.next_node = []                     # next nodes
             self.calculations_remaining = 0         # number of nodes needed to calculate derived score
             self.isExecCode = False                 # whether node is execCode node (used for percentage execCode metric)
-            self.tolNumConditions = 0               # sum total of conditions to reach node
-            self.numConditions = 0                  # number of conditions to reach node
+            self.tolNumConditions = 0               # total num of conditions to reach node
+            self.diNumConditions = 0                # direct number of conditions to reach node
 
         def printFunc(self):
             print(self.derived_score, self.description, self.node_type, self.node_logic, self.next_node, self.calculations_remaining, self.isExecCode)
 
+    # object for edges nodes will use
+    class Edge:
+        def __init__(self, node_source, node_target):
+            self.target = node_target    # target node (where the edge connect too)
+            self.source = node_source    # source node (where the edge starts)
+
+            self.target.in_edges.append(self)    # adding incoming edge to target
+            self.source.out_edges.append(self)
+
 def DataDriven_init():
     global LAG
+    global derived_score_computation_time
 
     LAG.clear()
-    derived_score_computation_time = None
+    derived_score_computation_time = 0.0
 
 '''
 Probability Formulas:
@@ -57,44 +75,38 @@ For any n events e1, e2, ..., en:
 '''
 
 # scores is derived scores tuple
-# key is dictionary key to access node
-def Depth_First_Alg(scores, numConditions, key): 
-    global LAG
-
+def Depth_First_Alg(scores, tolNumConditions, node): 
     # reduce number of nodes needed to make calculation
-    LAG[key].calculations_remaining -= 1
+    node.calculations_remaining -= 1
     
     # adding number of conditions to reach node
-    LAG[key].tolNumConditions += numConditions
-    
+    node.tolNumConditions += tolNumConditions
 
     # modifying score
-    if LAG[key].node_logic == DataDriven.Node_Logic.OR:
+    if node.node_logic == DataDriven.Node_Logic.OR:
         # OR = (1-p1)*...*(1-pn)
         for i in range(3):
-            LAG[key].derived_score[i] = LAG[key].derived_score[i]*(1-scores[i])     # probability formula 2
+           node.derived_score[i] = node.derived_score[i]*(1-scores[i])     # probability formula 2
     else: # node_log == AND OR FLOW
         # AND = p1*...*pn
         for i in range(3):
-            LAG[key].derived_score[i] = LAG[key].derived_score[i]*scores[i]         # probability formula 1
+            node.derived_score[i] = node.derived_score[i]*scores[i]         # probability formula 1
       
     # if no more nodes are required to make calculation
-    if LAG[key].calculations_remaining == 0:
+    if node.calculations_remaining == 0:
         # if OR node, then finalize calculation
         # 1 - (1-p1)*...*(1-pn)
-        if LAG[key].node_logic == DataDriven.Node_Logic.OR:
+        if node.node_logic == DataDriven.Node_Logic.OR:
             for i in range(3):
-                LAG[key].derived_score[i] = 1-LAG[key].derived_score[i]             # probability formula 2
+                node.derived_score[i] = 1-node.derived_score[i]             # probability formula 2
 
         # next node(s)
-        for k in LAG[key].next_node:
-            Depth_First_Alg(LAG[key].derived_score, LAG[key].numConditions, k)
+        for next_node in node.next_node:
+            Depth_First_Alg(node.derived_score, node.tolNumConditions, next_node)
 
-def DerivedScore(lag_dict, leaf_queue):
+def DerivedScore(leaf_queue):
     global LAG
     global derived_score_computation_time
-
-    LAG = lag_dict
     
     # starting timer for computation time
     start_time = time.time()
@@ -102,8 +114,8 @@ def DerivedScore(lag_dict, leaf_queue):
     # modifying derived scores
     while len(leaf_queue) > 0:
         node = leaf_queue.pop()
-        for key in node.next_node:
-            Depth_First_Alg(node.derived_score, 1, key)
+        for next_node in node.next_node:
+            Depth_First_Alg(node.derived_score, 1, next_node)
     
     # calculating computation time
     derived_score_computation_time = time.time() - start_time
@@ -122,53 +134,26 @@ def getDerivedScores():
 
     vertices = []
     edges = []
-    for key in LAG: 
+    for node in LAG: 
         vertices.append({
-                'id' : key,
-                'description' : LAG[key].description,
-                'node_type' : node_type_to_str[LAG[key].node_type], 
-                'base_score' : round(LAG[key].derived_score[0],3),
-                'exploitability_score' : round(LAG[key].derived_score[1],3),
-                'impact_score' : round(LAG[key].derived_score[2],3)})
-        for e in LAG[key].next_node:
-            edges.append({'source' : key, 'target' : e})
+                'id' : node.index,
+                'description' : node.description,
+                'node_type' : node_type_to_str[node.node_type], 
+                'base_score' : round(node.derived_score[0],3),
+                'exploitability_score' : round(node.derived_score[1],3),
+                'impact_score' : round(node.derived_score[2],3)})
+        for e in node.next_node:
+            edges.append({'source' : node.index, 'target' : e.index})
     
     return jsonify({'nodes': vertices, 'edges' : edges, "computation_time" : derived_score_computation_time})
 
-@data_analysis_bp.route('/data_driven/test-derived-scores', methods=['GET'])
-def test_Derived_Scores():
-    nodes = [{
-            'id': 1,
-            'description': 'Something!',
-            'node_type' : 'Leaf',
-            'base_score' : 10,
-            'exploitability_score': 5,
-            'impact_score': 7
-        },
-        {
-            'id': 2,
-            'description': 'Something Part 2!',
-            'node_type' : 'Leaf',
-            'base_score' : 8,
-            'exploitability_score': 4,
-            'impact_score': 3
-        }
-    ]
-
-    links = [{
-            'source': '1',
-            'target': '2'
-        }
-    ]
-
-    return jsonify({'nodes': nodes, 'edges': links}), 200
 #################### DATA-DRIVEN LAG Metrics ########################
 @data_analysis_bp.route('/data_driven/percentage_execCode_nodes', methods=['GET'])
 def percentage_execCode_nodes():
     global LAG
     sum = 0
-    for key in LAG:
-        sum += LAG[key].isExecCode
+    for node in LAG:
+        sum += node.isExecCode
 
     result=round((float(sum) / float(len(LAG)) * 100.0),3)
     print(sum)
@@ -180,15 +165,15 @@ def execCode_node_probabilities():
     global LAG
 
     vertices = []
-    for key in LAG: 
-        if LAG[key].isExecCode:
+    for node in LAG: 
+        if node.isExecCode:
             vertices.append({
-                'id' : key,
-                'description' : LAG[key].description,
+                'id' : node,
+                'description' : node.description,
                 'node_type' : 'Derived Fact', 
-                'base_score' : round(LAG[key].derived_score[0],3),
-                'exploitability_score' : round(LAG[key].derived_score[1],3),
-                'impact_score' : round(LAG[key].derived_score[2],3)})
+                'base_score' : round(node.derived_score[0],3),
+                'exploitability_score' : round(node.derived_score[1],3),
+                'impact_score' : round(node.derived_score[2],3)})
     
     return jsonify({'nodes': vertices})
 
@@ -198,15 +183,15 @@ def derived_node_probabilities():
     global LAG
 
     vertices = []
-    for key in LAG: 
-        if LAG[key].node_type == DataDriven.Node_Type.DERIVED:
+    for node in LAG: 
+        if node.node_type == DataDriven.Node_Type.DERIVED:
             vertices.append({
-                'id' : key,
-                'description' : LAG[key].description,
+                'id' : node,
+                'description' : node.description,
                 'node_type' : 'Derived Fact', 
-                'base_score' : round(LAG[key].derived_score[0],3),
-                'exploitability_score' : round(LAG[key].derived_score[1],3),
-                'impact_score' : round(LAG[key].derived_score[2],3)})
+                'base_score' : round(node.derived_score[0],3),
+                'exploitability_score' : round(node.derived_score[1],3),
+                'impact_score' : round(node.derived_score[2],3)})
     
     return jsonify({'nodes': vertices})
 
@@ -214,8 +199,8 @@ def derived_node_probabilities():
 def percentage_rule_nodes():
     global LAG
     rules = 0
-    for key in LAG:
-        rules += (LAG[key].node_type == DataDriven.Node_Type.DERIVATION)
+    for node in LAG:
+        rules += (node.node_type == DataDriven.Node_Type.DERIVATION)
 
     result=round((float(rules) / float(len(LAG)) * 100.0),3)
     return jsonify({'percentage_rule_nodes': result})
@@ -224,8 +209,8 @@ def percentage_rule_nodes():
 def percentage_derived_nodes():
     global LAG
     numDerived = 0
-    for key in LAG:
-        numDerived += (LAG[key].node_type == DataDriven.Node_Type.DERIVED)
+    for node in LAG:
+        numDerived += (node.node_type == DataDriven.Node_Type.DERIVED)
     
     result=round((float(numDerived) / float(len(LAG)) * 100.0),3)
     return jsonify({'percentage_derived_nodes': result})
@@ -234,9 +219,9 @@ def percentage_derived_nodes():
 def network_entropy():
     global LAG
     net_entropy = [0.0,0.0,0.0]
-    for key in LAG:
+    for node in LAG:
         for i in range(3):
-            net_entropy[i] += LAG[key].derived_score[i] * math.log2(LAG[key].derived_score[i])
+            net_entropy[i] += node.derived_score[i] * math.log2(node.derived_score[i])
     
     for i in range(3):
         net_entropy[i] *= -1.0
@@ -254,11 +239,11 @@ def conditions_per_derived_nodes():
     global LAG
 
     conditions_derived = []
-    for key in LAG:
-        if LAG[key].node_type == DataDriven.Node_Type.DERIVED:
+    for node in LAG:
+        if node.node_type == DataDriven.Node_Type.DERIVED:
             conditions_derived.append({
-                "id" : key,
-                "num_conditions" : LAG[key].tolNumConditions # total number of conditions to reach node
+                "id" : node,
+                "num_conditions" : node.tolNumConditions # total number of conditions to reach node
             })
     
     return jsonify({"conditions_per_derived_node" : conditions_derived})
@@ -269,11 +254,11 @@ def conditions_per_execCode_node():
     global LAG
 
     conditions_derived = []
-    for key in LAG:
-        if LAG[key].isExecCode:
+    for node in LAG:
+        if node.isExecCode:
             conditions_derived.append({
-                "id" : key,
-                "num_conditions" : LAG[key].tolNumConditions # total number of conditions to reach node
+                "id" : node,
+                "num_conditions" : node.tolNumConditions # total number of conditions to reach node
             })
     
     return jsonify({"conditions_per_execCode_node" : conditions_derived})
@@ -284,11 +269,11 @@ def conditions_per_execCode_node():
     global LAG
 
     rules_derived = []
-    for key in LAG:
-        if LAG[key].node_type == DataDriven.Node_Type.DERIVED:
+    for node in LAG:
+        if node.node_type == DataDriven.Node_Type.DERIVED:
             rules_derived.append({
-                "id" : key,
-                "num_conditions" : LAG[key].numConditions # number of rules
+                "id" : node,
+                "num_conditions" : node.diNumConditions # direct number of rules
             })
     
     return jsonify({"rules_per_derived_node" : rules_derived})
@@ -299,11 +284,11 @@ def conditions_per_execCode_node():
     global LAG
 
     rules_derived = []
-    for key in LAG:
-        if LAG[key].isExecCode:
+    for node in LAG:
+        if node.isExecCode:
             rules_derived.append({
-                "id" : key,
-                "num_conditions" : LAG[key].numConditions   # number of rules
+                "id" : node,
+                "num_conditions" : node.diNumConditions   # direct number of rules
             })
     
     return jsonify({"rules_per_execCode_node" : rules_derived})
